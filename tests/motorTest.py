a5a056212ab851cc90c87ea6d0b9773e0f7546fd
Chris <chris@pop-os.localdomain>
2021-12-12 15:56:34 +0100

    added test for motor controller

---
+++ tests/motorTest.py
@@ -0,0 +1,21 @@
+from drive import motorControl
+from pubsub import pub
+from time import time
+
+class motorTest:
+    def __init__(self):
+        self.runTests()
+
+    def runTests(self):
+        pub.sendMessage('moveDirection', moveDirection='left')
+        time.delay(3000)
+        pub.sendMessage('moveDirection', moveDirection='right')
+        time.delay(3000)
+        pub.sendMessage('moveDirection', moveDirection='forwards')
+        time.delay(3000)
+        pub.sendMessage('moveDirection', moveDirection='reverse')
+        time.delay(3000)
+
+if __name__ == "__main__":
+    motorControl = motorControl.motorControl()
+    test = motorTest()
